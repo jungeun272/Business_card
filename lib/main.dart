15aa47d2d1e4bfd9ce37acb851400c1905b02f0b
jungeun272 <hanjungeun272@gmail.com>
2024-04-04 01:06:59 +0900

    Create Buisness_Card

---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      ),
+      home: Scaffold(
+        body: Container(
+          width: 390,
+          height: 844,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Container(
+                width: 390,
+                height: 55,
+                child: const Align(
+                  alignment: Alignment.center, // 상단 중앙에 위치
+                  child: Text(
+                    'Business card',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              ClipOval( //동그랗게만 만들어주는거임
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color(0XFFF5F5F5),
+                  child: const Icon(Icons.person_3, size: 54, color: Color(0XFFD1D5DB),),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Container(
+                width: 390,
+                child: const Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Jungeun Han',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text('flutter developer')
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 16, right: 16),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(color: Color(0xFFF0F0F0), width: 1),
+                        ),
+                      ),
+                      height: 60,
+                      child: const Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.phone),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text('010-1234-5678'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(color: Color(0xFFF0F0F0), width: 1),
+                        ),
+                      ),
+                      height: 60,
+                      child: const Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.send),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text('@jungle'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(color: Color(0xFFF0F0F0), width: 1),
+                        ),
+                      ),
+                      height: 60,
+                      child: const Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.email),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text('jungeun@gmail.com'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
